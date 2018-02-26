4202dba121524aef9add889a52e425a55fb5d25f
kevinlien <za955120@hotmail.com>
2018-02-26 14:02:05 -0800

    Initial Commit

---
+++ Concentrate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentrate
+//
+//  Created by kevinlien on 2018/2/26.
+//  Copyright © 2018年 Lien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
